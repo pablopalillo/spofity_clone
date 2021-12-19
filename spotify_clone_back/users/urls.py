3354f12322d5f2f055c9f74371adafbb9acf554d
pablopalillo <pablo.576@hotmail.com>
2021-12-18 22:01:57 -0500

    Users implementations:
        - url mapping
        - User Register with username, email, password
        - User login token with token DRF
        - User logout with token DRF

--- spotify_clone_back/users/urls.py
+++ spotify_clone_back/users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 
 from . import views
-
+from rest_framework.authtoken import views as token_views
 
 urlpatterns = [
-   path('api/user', views.UserLogin.as_view()),
+   path('api/user', views.RegisterView.as_view()),
+   path('api/register', views.RegisterView.as_view()),
+   path('api/api-token-auth', token_views.obtain_auth_token),
+   path('api/api-token-logout', views.LogoutView.as_view())
 ]
--- spotify_clone_back/users/views.py
+++ spotify_clone_back/users/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
+from rest_framework import permissions
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import UserSerializer
+
+
+class RegisterView(APIView):
+
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+
+        try:
+            user_serializer = UserSerializer(data=request.data)
+            if user_serializer.is_valid(raise_exception=True):
+
+                User.objects.create_user(
+                    username=request.data['username'],
+                    email=request.data['email'],
+                    password=request.data['password']
+                )
+
+                return Response(user_serializer.data, status.HTTP_201_CREATED)
+
+        except ObjectDoesNotExist:
+            raise Http404
+
+
+class LogoutView(APIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+
+        try:
+            request.user.auth_token.delete()
+            return Response(status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            raise Http404
