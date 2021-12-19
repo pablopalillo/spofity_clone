48f2ff45336525c6aec631e454276246708aecdd
pablopalillo <pablo.576@hotmail.com>
2021-12-18 22:44:58 -0500

    Podcast model

--- spotify_clone_back/podcasts/models.py
+++ spotify_clone_back/podcasts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Podcast(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=150)
+    description = models.TextField(null=True)
+    author = models.CharField(max_length=45, null=True)
+    create_date = models.DateField()
+
+    def __str__(self):
+        return self.name
